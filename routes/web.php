7699622cb34b536f831cb98af2794283124f7141
Azizbek Isroilov <aziz16110904@gmail.com>
2024-03-01 15:33:22 +0500

    laravel sitemap example

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +19,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::resource('posts', PostController::class);
+
+Route::get('/sitemap', function () {
+    $sitemap = Sitemap::create()
+        ->add(Url::create('/home'))
+        ->add(Url::create('/about'))
+        ->add(Url::create('/contact'));
+    Post::all()->each(function (Post $post) use ($sitemap) {
+        $sitemap->add(Url::create("/posts/{$post->slug}"));
+    });
+    $sitemap->writeToFile(public_path('sitemap.xml'));
+
+    return 'Sitemap create successfully';
 });
